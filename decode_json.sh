fc08eb8a387d65a76c9e8e5b8b21876e65f18019
Chiri Miura <77763432+chirimiura@users.noreply.github.com>
2023-03-08 17:51:15 +0900

    Update decode_json.sh

--- decode_json.sh
+++ decode_json.sh
@@ -1,21 +1,23 @@
-require "json"
-                                                                              
-###変数宣言
-#APIの返却データ数をカウント                                                  
-cnt = 0                                                                       
-    
-File.open(ARGV[0]+".tmp.json") do |f|
-  data = JSON.load(f)
-  #p data
-                                                                              
-  for t in data do
-    if t['id'] != '' then 
-      cnt = cnt+1                                                             
-    end                                                                       
-    if t['type'] == 'PullRequestEvent' then                                   
-      print t['payload']['pull_request']['base']['user']['login']+','+t['payload']['pull_request']['base']['repo']['language']+','+t['payload']['pull_request']['url']+"\n"
-    end 
-  end
-end                                                                           
-  
-p cnt
+token=$GIT_TOKEN #GitHubのトークンを設定
+
+for f in "$@"
+do
+  curl \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $token"\
+    "https://api.github.com/users/$f/events/public?per_page=100" > /Users/cmiura/Documents/tmp_githubdata/$f.tmp.json
+
+  cd /Users/cmiura/Documents/tmp_githubdata
+  ruby json_parse.rb $f > /Users/cmiura/Documents/tmp_githubdata/$f.tmp.json.parse
+
+
+  #ブラウザで結果を見たいとき
+  #open /Users/cmiura/Documents/tmp_githubdata/$f.tmp.json.parse -a /Applications/Google\ Chrome.app
+
+  #コマンドラインで結果を見たいとき
+  while read line
+  do
+    echo $line
+  done < /Users/cmiura/Documents/tmp_githubdata/$f.tmp.json.parse
+done
+
