70c89021d0b215d7947ee816e74342908edf994c
Chiri Miura <77763432+chirimiura@users.noreply.github.com>
2023-03-08 17:50:09 +0900

    Update json_parse.rb

--- json_parse.rb
+++ json_parse.rb
@@ -1,12 +1,21 @@
 require "json"
-
+                                                                              
+###変数宣言
+#APIの返却データ数をカウント                                                  
+cnt = 0                                                                       
+    
 File.open(ARGV[0]+".tmp.json") do |f|
   data = JSON.load(f)
-  # p data
-
+  #p data
+                                                                              
   for t in data do
-    if t['type'] == 'PullRequestEvent' then
+    if t['id'] != '' then 
+      cnt = cnt+1                                                             
+    end                                                                       
+    if t['type'] == 'PullRequestEvent' then                                   
       print t['payload']['pull_request']['base']['user']['login']+','+t['payload']['pull_request']['base']['repo']['language']+','+t['payload']['pull_request']['url']+"\n"
-    end
+    end 
   end
-end
+end                                                                           
+  
+p cnt
